05e2bfbf752b206cfc0fd79ec090856037a8a301
Benjamin Pire <bpire@wmo.int>
2024-01-12 18:28:31 +0100

    last version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cop_28_game_app
 Game application for COP 28
+
+
+# localy
+
+
+# remote 
--- pages/page_start.py
+++ pages/page_start.py
@@ -4,30 +4,29 @@ import pandas as pd
 import dash
 
 #page_start = dash.register_page(__name__, path="/page_start")
+wmo_logo = 'assets/wmo_logo.jpg'
+decadal_report_logo = 'assets/decadal.png'
 
 
 layout = html.Div([
-    html.H1(children='WMO Report Game', className='title'),
+    html.Img(src=wmo_logo, style={'height':'150px', 'width':'150px'}),
     html.Div([
-
         html.Div([
-            html.H3('Age'),            
-            dcc.Input(id='user-age', 
-                      type='text', 
-                      placeholder="",
-                      className='input')],
-                 className='start-box'),
-
-        html.Div([
-            html.H3('Current ocupation'),            
-            dcc.Input(id='user-prof', 
-                      type='text',
-                      placeholder="",
-                      className='input')], 
-                 className='start-box'),
-    ],  style={'display': 'flex', 'flex-direction': 'row'} ),
+            html.H5(children='The WMO game'),
+            html.H1(children='Do you want to understand climate change and its consequences ?')],
+            style={'display':'flex', 'flex-direction':'column'},
+            className='question_text')],
+        className='start-box'),
+    html.Div([
+        html.Button(children=html.Img(src=decadal_report_logo, style={'height':'200px', 'width':'200px'}),
+                    id='decadal_version', 
+                    n_clicks=0, 
+                    className='button_version')],
+        className='start-box'),
     html.Button('Start', 
                 id='start-button', 
                 n_clicks=0, 
-                className='button')])
+                className='button')], className='general')
+
+
 
